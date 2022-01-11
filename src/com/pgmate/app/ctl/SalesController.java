d50989ea317123647e40606f11d0faf5bc7483d2
boram0415 <boram0415@naver.com>
2022-01-11 19:52:59 +0900

    Seles / settle merge

--- src/com/pgmate/app/ctl/SalesController.java
+++ src/com/pgmate/app/ctl/SalesController.java
@@ -49,27 +49,28 @@ public class SalesController {
 		RecordSet rset = totCapDAO.salesIssuerList(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,totCapDAO).setView(request,"/sales/issuer/list","");
 	}
-	
+	// KBR 현재 사용 안함
 	@RequestMapping(value = "/sales/daily/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsDailyList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
 		request.setAttribute("SUMMAP", new TotCapDAO().salesSum(cpRequest.data).getRow(0));
-		
 		TotCapDAO totCapDAO = new TotCapDAO();
 		RecordSet rset = totCapDAO.salesDailyList(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,totCapDAO).setView(request,"/sales/daily/list","");
 	}
 	
+	// KBR 월별 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/monthly/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsMonthlyList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
 		request.setAttribute("SUMMAP", new TotCapMonthlyDAO().salesSum(cpRequest.data).getRow(0));
-		
+		// 총 합 계산 쿼리 셋팅 
 		TotCapMonthlyDAO totCapMonthlyDAO = new TotCapMonthlyDAO();
 		RecordSet rset = totCapMonthlyDAO.salesMonthlyList(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,totCapMonthlyDAO).setView(request,"/sales/monthly/list","");
 	}
 	
+	// KBR 가맹점 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/mcht/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsMchtList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
@@ -79,7 +80,7 @@ public class SalesController {
 		RecordSet rset = totCapDayDAO.salesMchtDailyList(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,totCapDayDAO).setView(request,"/sales/mcht/list","");
 	}
-	
+	// KBR 월별 가맹점 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/mchtMonthly/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsMchtMonthlyList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
@@ -89,7 +90,7 @@ public class SalesController {
 		RecordSet rset = totCapMonthlyDAO.salesMchtMonthlyList(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,totCapMonthlyDAO).setView(request,"/sales/mchtMonthly/list","");
 	}
-	
+	// KBR 에이전시 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/agency/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsAgencyList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
@@ -100,6 +101,7 @@ public class SalesController {
 		return new CPRUtil(cpRequest).dataList(rset,totAgencyCapDAO).setView(request,"/sales/agency/list","");
 	}
 	
+	// KBR 대행사 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/dist/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsDistList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
@@ -110,6 +112,7 @@ public class SalesController {
 		return new CPRUtil(cpRequest).dataList(rset,totCapDistDAO).setView(request,"/sales/dist/list","");
 	}
 	
+	// KBR 지사 매출관리 클릭 시 
 	@RequestMapping(value = "/sales/sales/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView saelsSalesList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		SessionUtil.setSearchGrade(request, cpRequest);
@@ -180,14 +183,19 @@ public class SalesController {
 	//정산일기준 일마감 통계  dailysByStlDay
 	@RequestMapping(value="/sales/dailysByStlDay/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView dailysByStlDay(HttpServletRequest request, @RequestBody CPRequest cpRequest) {
+		
+		// KBR : OrderBy("days desc") 셋팅
 		TrxDailyDAO trxDailyDAO = new TrxDailyDAO();
+		
 		RecordSet rset = trxDailyDAO.dailysByStlDay(cpRequest.data, cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset, trxDailyDAO).setView(request, "/sales/dailysByStlDay/list", "");
 	}
 	
 	//정산일기준 일마감 통계  dailysByStlDay
+	// B: 정산일기준 일마감 통계  > jsp 페이지 내 기준일 클릭 시 모달 창
 	@RequestMapping(value="/sales/dailysByStlDay/dtl/form/{stlDay}", method=RequestMethod.GET)
 	public ModelAndView dailysByStlDayDTL(HttpServletRequest request, @PathVariable String stlDay) {
+		// 모달창에 뿌려줄 기준일에 매입된 모든 리스트 
 		return new ModelAndView("/sales/dailysByStlDay/dtl/form", "DATAMAP", new TrxDailyDAO().dailysByStlDayDTL(stlDay));
 	}
 	
@@ -199,6 +207,7 @@ public class SalesController {
 		return new CPRUtil(cpRequest).dataList(rset,trxDailyDAO).setView(request,"/sales/dailysByCapDay/list","");
 	}
 	//승인일기준 일마감 통계 가맹점 기준
+	// B: 승인일기준 일마감 통계  > jsp 페이지 내 기준일 클릭 시 모달 창
 	@RequestMapping(value = "/sales/dailysByCapDay/dtl/{capDay}", method = RequestMethod.GET)
     public ModelAndView viewTabByCapDay(HttpServletRequest request, @PathVariable String capDay) {
         return new ModelAndView("/sales/dailysByCapDay/dtl/form", "DATAMAP", new TrxDailyDAO().getDailyCapByCapDay(capDay));
@@ -214,6 +223,7 @@ public class SalesController {
 		return new ModelAndView("/sales/collect/list", "DATAMAP", new TrxDailyDAO().getCollect(cpRequest.getKeyValue("startCollectDay"), cpRequest.getKeyValue("endCollectDay")));
 	}
 	
+	// KBR 개맹점별 입금정산 내역 클릭 시 
 	@RequestMapping(value = "/sales/collect/mcht/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView salesCollectMcht(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
 		RecordSet rset = new TrxDailyDAO().getCollectWithMcht(cpRequest.data);
@@ -249,8 +259,11 @@ public class SalesController {
 		dao.setDebug(true);
 		dao.setTable("VW_TRX_CAP_CALENDAR");
 		dao.setColumns(sb.toString());
+		
+		// 본사가 아닐경우 ( 근데 이건 본사 이외 볼수있는 권한이 없는데..)
 		if(whereStr != "") {
 			dao.setWhere("regDay > DATE_FORMAT(LAST_DAY(date_add(NOW(), interval -1 month)), '%Y%m%d') and regDay <= DATE_FORMAT(LAST_DAY(NOW()), '%Y%m%d') AND " + whereStr + " = '" + cpSession.getParentId() + "'");
+			// 본사일 경우
 		} else {
 			dao.setWhere("regDay > DATE_FORMAT(LAST_DAY(date_add(NOW(), interval -1 month)), '%Y%m%d') and regDay <= DATE_FORMAT(LAST_DAY(NOW()), '%Y%m%d')");
 		}
@@ -262,12 +275,15 @@ public class SalesController {
 //		}
 	}
 	
-	
+	// B:정산일정 클릭 시 (총 2곳 호출 ↓) : 왜 따로 썼을까..?
 	@RequestMapping(value = "/sales/settle/getCalendar", method = RequestMethod.GET)
 	public @ResponseBody List<SharedMap<String, Object>> getCalendar (HttpServletRequest request) {
 		
 		List<SharedMap<String, Object>> result = new ArrayList<SharedMap<String, Object>>();
+		
 		CPDAO settleDAO = new CPDAO();
+		
+		// 일자별 입금,청산,차액 값 가져오는 쿼리문 셋팅
 		StringBuffer query = new StringBuffer();
 		query.append("select A.*,B.*, A.stlVanAmount - B.stlAmount AS difference from ");
 		query.append("(select SUM(stlVanAmount) as stlVanAmount, stlVanDay from VW_TRX_CAP_CALENDAR ");
@@ -276,9 +292,32 @@ public class SalesController {
 		query.append("where regDay > DATE_FORMAT(LAST_DAY(NOW() - interval 2 month),'%Y%m%d') and stlDay > DATE_FORMAT(LAST_DAY(NOW() - interval 1 month),'%Y%m%d') AND stlDay <= DATE_FORMAT(LAST_DAY(NOW()),'%Y%m%d') ");
 		query.append("group by stlDay) B on A.stlVanDay = B.stlDay ");
 		
+		// KBR : 쿼리 정리 
+//		select A.*,B.*, A.stlVanAmount - B.stlAmount AS difference 
+//		from 
+//		(select SUM(stlVanAmount) as stlVanAmount, stlVanDay from vw_trx_cap_calendar 
+//		where regDay > DATE_FORMAT(LAST_DAY(NOW() - interval 2 month),'%Y%m%d') AND 
+//		stlVanDay > DATE_FORMAT(LAST_DAY(NOW() - interval 1 month),'%Y%m%d') AND 
+//		stlVanDay <= DATE_FORMAT(LAST_DAY(NOW()),'%Y%m%d') 
+//		group by stlVanDay) A 
+//
+//		LEFT JOIN 
+//
+//		(select SUM(stlAmount) as stlAmount, stlDay 
+//		from vw_trx_cap_calendar 
+//		where regDay > DATE_FORMAT(LAST_DAY(NOW() - interval 2 month),'%Y%m%d') AND 
+//		stlDay > DATE_FORMAT(LAST_DAY(NOW() - INTERVAL 1 month),'%Y%m%d') AND 
+//		stlDay <= DATE_FORMAT(LAST_DAY(NOW()),'%Y%m%d') 
+//		group by stlDay) B 
+//		on A.stlVanDay = B.stlDay 
 		
+		
+		// ↑ 쿼리 실행 후 결과 값 셋팅
 		List<SharedMap<String, Object>> resList = settleDAO.query(query.toString()).getRows();
+		// 초기화 ( 왜 초기화 하는겨..?)
 		settleDAO.initRecord();
+		
+		// jsp 화면에 표시 할 입금금액 ,  
 		for (SharedMap<String, Object> each : resList) {
 			if (each.getLong("stlVanAmount") > 0) {
 				NumberFormat nf = NumberFormat.getInstance();
@@ -319,13 +358,15 @@ public class SalesController {
 		return result;
 	}
 
-	
+	// B:정산일정 클릭 시 (총 2곳 호출 ↑)
 	@RequestMapping(value = "/sales/settle/getSettleData", method = RequestMethod.GET)
 	public @ResponseBody SharedMap<String, Object> getSettleData (HttpServletRequest request ) {
 		
 		SharedMap<String, Object> resultMap = new SharedMap<String, Object>();
 		
 		CPSession cpSession = SessionUtil.get(request);
+		
+		// 오늘 일자와  승인내역  ~ 이번달 취소내역 리스트 
 		StringBuffer sb = new StringBuffer();
 		sb.append("DATE_FORMAT(NOW(),'%Y%m%d') as today,");
 		sb.append("FORMAT(SUM(IF(capType = '매입' AND regDay = DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS dayPay,");
@@ -336,6 +377,22 @@ public class SalesController {
 		sb.append("FORMAT(SUM(IF(capType = '매입' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS monthPayCnt,");
 		sb.append("FORMAT(SUM(IF(capType = '매입취소' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS monthRef,");
 		sb.append("FORMAT(SUM(IF(capType = '매입취소' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS monthRefCnt");
+		
+		// KBR : 쿼리문 
+//		SELECT 
+//		DATE_FORMAT(NOW(),'%Y%m%d') as today,
+//		FORMAT(SUM(IF(capType = '매입' AND regDay = DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS dayPay,
+//		FORMAT(SUM(IF(capType = '매입' AND regDay = DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS dayPayCnt,
+//		FORMAT(SUM(IF(capType = '매입취소' AND regDay = DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS dayRef,
+//		FORMAT(SUM(IF(capType = '매입취소' AND regDay = DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS dayRefCnt,
+//		FORMAT(SUM(IF(capType = '매입' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS monthPay,
+//		FORMAT(SUM(IF(capType = '매입' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS monthPayCnt,
+//		FORMAT(SUM(IF(capType = '매입취소' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), amount, 0)),0) AS monthRef,
+//		FORMAT(SUM(IF(capType = '매입취소' AND regDay >= DATE_FORMAT(NOW(),'%Y%m01') AND regDay <= DATE_FORMAT(NOW(),'%Y%m%d'), 1, 0)),0) AS monthRefCnt 
+//		FROM vw_trx_cap_calendar 
+//		WHERE  regDay > DATE_FORMAT(LAST_DAY(date_add(NOW(), interval -1 month)), '%Y%m%d') 
+//		and regDay <= DATE_FORMAT(LAST_DAY(NOW()), '%Y%m%d') 
+//		ORDER BY regDate DESC
 		
 		String whereStr = "";
 		if (cpSession.getGrade().equals("대행사")) {
@@ -349,11 +406,15 @@ public class SalesController {
 		}
 		
 		CPDAO dao = new CPDAO();
+		
 		dao.setDebug(true);
 		dao.setTable("VW_TRX_CAP_CALENDAR");
 		dao.setColumns(sb.toString());
+		
+		// 본사가 아닐경우 (본사 외 다른 업체는 정산일정 볼수가 없는데..)
 		if(whereStr != "") {
 			dao.setWhere("regDay > DATE_FORMAT(LAST_DAY(date_add(NOW(), interval -1 month)), '%Y%m%d') and regDay <= DATE_FORMAT(LAST_DAY(NOW()), '%Y%m%d') AND " + whereStr + " = '" + cpSession.getParentId() + "'");
+			// 본사일 경우
 		} else {
 			dao.setWhere("regDay > DATE_FORMAT(LAST_DAY(date_add(NOW(), interval -1 month)), '%Y%m%d') and regDay <= DATE_FORMAT(LAST_DAY(NOW()), '%Y%m%d')");
 		}
@@ -416,20 +477,6 @@ public class SalesController {
 		}
 		request.setAttribute("SETTLEJSON", GsonUtil.toJson(result, false, ""));
 	}
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
