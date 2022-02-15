320b1c2d2ea3e4fa30127c8e38e05f9e6cc66c84
pys890102 <pys890102@bkwinnners.com>
2022-02-15 16:27:40 +0900

    refund URL 변경

--- src/com/pgmate/app/util/RefundUtil.java
+++ src/com/pgmate/app/util/RefundUtil.java
@@ -142,8 +142,8 @@ public class RefundUtil {
 	}
 	
 	public String comm(String request,String payKey){
-		String paymentUrl = "https://api.bkwinners.kr/api/refund";
-		//String paymentUrl = "http://pgwas1:10002/api/refund";
+		//String paymentUrl = "https://api.bkwinners.kr/api/refund";
+		String paymentUrl = "http://pgwas2:10002/api/refund";
 		//String paymentUrl = "http://127.0.0.1:10002/api/refund";
 		StringBuilder result = new StringBuilder();
 		URL url = null;
