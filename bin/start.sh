7d6699532c6fcf582977ec883176bfda65ea2da3
pys890102 <pys890102@bkwinnners.com>
2022-04-01 10:04:48 +0900

    start.sh 변경 (엑셀다운기능 추가)

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 
 # JVM_ARGS for VM
 ##########################
-JVM_ARGS="-DMARU_APP -server -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8"
+JVM_ARGS="-DMARU_APP -server -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8 -Djava.io.tmpdir=../web/upload -Djava.awt.headless=true"
 JVM_ARGS="$JVM_ARGS -Xss512k -Xms1024m -Xmx2048m"
 JVM_ARGS="$JVM_ARGS -cp ../lib/*:../lib/apache/*:../lib/tomcat/*:../lib/spring/*:../web/WEB-INF/classes"
 
