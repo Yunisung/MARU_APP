b16416bb84f0a0352fd27f4feb28fdaf1dbe0acc
pys890102 <pys890102@bkwinnners.com>
2022-04-19 15:33:55 +0900

    개발서버 신규등록

--- src/com/pgmate/app/util/RefundUtil.java
+++ src/com/pgmate/app/util/RefundUtil.java
@@ -254,9 +254,10 @@ public class RefundUtil {
 	
 	public String phoneRef(String request,String payKey){
 		//개발
-		//String paymentUrl = "https://svcapidev.mtouch.com/api/phone/refund";
+		String paymentUrl = "https://devapi.bkwinners.kr/api/phone/refund";
 		//운영
-		String paymentUrl = "http://api.bkwinners.kr/api/phone/refund";
+//		String paymentUrl = "https://api.bkwinners.kr/api/phone/refund";
+		//로컬
 		//String paymentUrl = "http://127.0.0.1:10002/api/phone/refund";
 		
 		StringBuilder result = new StringBuilder();
