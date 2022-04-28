30504d6db8d472476b7f7f1077a3ccffa3980bf9
pys890102 <pys890102@bkwinnners.com>
2022-04-28 13:24:12 +0900

    다시되게

--- src/com/pgmate/app/util/RefundUtil.java
+++ src/com/pgmate/app/util/RefundUtil.java
@@ -144,7 +144,7 @@ public class RefundUtil {
 	public String comm(String request,String payKey){
 		//String paymentUrl = "https://api.bkwinners.kr/api/refund";
 		//String paymentUrl = "http://pgwas1:10002/api/refund";
-		String paymentUrl = "https://10.100.200.10:10002/api/refund";
+		String paymentUrl = "http://127.0.0.1:10002/api/refund";
 		StringBuilder result = new StringBuilder();
 		URL url = null;
 		HttpURLConnection conn = null;
