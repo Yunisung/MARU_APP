6c04fe8cf3ad4ec6a3b1ecd7b339253b231bedd4
pys890102 <pys890102@bkwinnners.com>
2022-08-11 15:38:28 +0900

    젠킨스 테스트

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bin/stop.sh
+./stop.sh
 
 # JVM_ARGS for VM
 ##########################
@@ -10,4 +10,4 @@ JVM_ARGS="$JVM_ARGS -cp ../lib/*:../lib/apache/*:../lib/tomcat/*:../lib/spring/*
 
 
 java $JVM_ARGS com.pgmate.lib.tomcat.Tomcat8 &
-echo $!>bin/pwd.pid
+echo $!>pwd.pid
