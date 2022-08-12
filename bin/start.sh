8d78a687bed4c7b0cbb80360ce3a16c7388c94f3
yangwenry <yangwenry@bkwinners.com>
2022-08-12 17:28:16 +0900

    배치파일 변경222

--- bin/start.sh
+++ bin/start.sh
@@ -8,6 +8,7 @@ JVM_ARGS="$JVM_ARGS -Xss512k -Xms1024m -Xmx2048m"
 JVM_ARGS="$JVM_ARGS -cp ./lib/*:./lib/apache/*:./lib/tomcat/*:./lib/spring/*:./web/WEB-INF/classes"
 
 
-java $JVM_ARGS com.pgmate.lib.tomcat.Tomcat8 &
+#java $JVM_ARGS com.pgmate.lib.tomcat.Tomcat8 &
+java $JVM_ARGS com.pgmate.lib.tomcat.Tomcat8
 echo $!>pwd.pid
 echo MARU_APP [START]
