de84de5fcf61f368770f41e650b25138adb77042
pys890102 <pys890102@bkwinnners.com>
2022-09-02 14:49:17 +0900

    펌서버로 두번보내는거 수정
    (cherry picked from commit ead41f8ae370f7fee7a0d137992ca96e478e5e3b)
    (cherry picked from commit 15d5d1aef586da04d969c479e9ee4f4dca94e232)

--- src/com/pgmate/app/util/FirmUtil.java
+++ src/com/pgmate/app/util/FirmUtil.java
@@ -36,7 +36,7 @@ public class FirmUtil {
 	  logger.info("요청:{}",action);
 	  firmBean = comm(firmBean);
 	  logger.info("응답:{},{}",firmBean.resultCd,firmBean.resultMsg);
-	  return comm(firmBean);
+	  return firmBean;
   }
   
 	/**
