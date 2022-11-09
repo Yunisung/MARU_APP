c26059d559dfab88c31667f1b404d7f4cb0ecce6
kjm <jjggooo>
2022-11-09 20:45:17 +0900

    HT_VACT_DTL INSERT

--- src/com/pgmate/app/dao/VactDtlDAO.java
+++ src/com/pgmate/app/dao/VactDtlDAO.java
@@ -2,6 +2,7 @@ package com.pgmate.app.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.pgmate.app.model.ajax.Data;
@@ -52,7 +53,7 @@ public class VactDtlDAO extends DAO {
 		return super.searchList(page.current, page.size, page.hash); //LIST PAGING
 	}
 
-	public int insert(String mchtId, String bankCd, long cnt, String holderName, String userId) {
+	public List<String> insert(String mchtId, String bankCd, long cnt, String holderName, String userId) {
 		super.setTable("VW_VACT_UNUSED");
 		super.setColumns("*");
 		super.addWhere("bankCd", bankCd);
@@ -67,6 +68,8 @@ public class VactDtlDAO extends DAO {
 		}
 
 		int inserted = 0;
+		ArrayList<String> issueIdList = new ArrayList<>();
+
 		logger.debug("insert PG_VACT_DTL batch: {}", targetList.size());
 		String query = "INSERT INTO PG_VACT_DTL (issueId, account, vactType, status, mchtId, holderName, amount, oper, expireAt, regId, regDay) "
 		+ "VALUES (?,?,?,?,?,?,?,?,?,?,?);";
@@ -84,9 +87,10 @@ public class VactDtlDAO extends DAO {
 			int count = 0;
 			String expire = (Integer.parseInt(CommonUtil.getCurrentDate("yyyy")) + 1) + CommonUtil.getCurrentDate("MMdd");
 
+			int i = 1;
 			for (SharedMap<String, Object> map : targetList) {
 				String issueId = TrxDAO.getIssueId();
-				int i = 1;
+				issueIdList.add(issueId);
 				pstmt.setString(i++, issueId);
 				pstmt.setString(i++, map.getString("account"));
 				pstmt.setString(i++, "영구");
@@ -99,6 +103,7 @@ public class VactDtlDAO extends DAO {
 				pstmt.setString(i++, userId);
 				pstmt.setString(i++, CommonUtil.getCurrentDate("yyyyMMdd"));
 				pstmt.addBatch();
+
 				if (++count % batchSize == 0) {
 					inserted += pstmt.executeBatch().length;
 				}
@@ -112,7 +117,92 @@ public class VactDtlDAO extends DAO {
 			db.close(pstmt);
 			db.close(conn);
 		}
+
+		//insert가 정상적으로 되면 issueIdList 반환
+		if(inserted > 0) {
+			return issueIdList;
+		}
+		return null;
+	}
+
+	/**
+	 * HT_VACT_DTL테이블에 저장
+	 * @param issueId	가상계좌발급번호
+	 * @return
+	 */
+	public int insertHtVactDtl(List<String> issueIdList){
+		logger.debug("insert HT_VACT_DTL batch: {}", issueIdList.size());
+		String query = "INSERT INTO HT_VACT_DTL (issueId, account, vactType, status, mchtId, holderName, amount, oper, trackId, depositCnt, depositLimitCnt, expireAt, expireDate, udf1, udf2, reason, resultCd, resultMsg, regId, regDay)"
+				+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+		DBManager db = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int inserted = 0;
+
+		try {
+			db = DBFactory.getInstance();
+			conn = db.getConnection();
+			pstmt = conn.prepareStatement(query);
+
+			int batchSize = 100;
+			int count = 0;
+			String expire = (Integer.parseInt(CommonUtil.getCurrentDate("yyyy")) + 1) + CommonUtil.getCurrentDate("MMdd");
+
+			int i = 1;
+			for (String id : issueIdList) {
+				SharedMap<String,Object> map = getVactDtl(id);
+				pstmt.setString(i++, map.getString("issueId"));
+				pstmt.setString(i++, map.getString("account"));
+				pstmt.setString(i++, map.getString("vactType"));
+				pstmt.setString(i++, map.getString("status"));
+				pstmt.setString(i++, map.getString("mchtId"));
+				pstmt.setString(i++, map.getString("holderName"));
+				pstmt.setString(i++, map.getString("amount"));
+				pstmt.setString(i++, map.getString("oper"));
+				pstmt.setString(i++, map.getString("trackId"));
+				pstmt.setString(i++, map.getString("depositCnt"));
+				pstmt.setString(i++, map.getString("depositLimitCnt"));
+				pstmt.setString(i++, map.getString("expireAt"));
+				pstmt.setString(i++, map.getString("expireDate"));
+				pstmt.setString(i++, map.getString("udf1"));
+				pstmt.setString(i++, map.getString("udf2"));
+				pstmt.setString(i++, map.getString("reason"));
+				pstmt.setString(i++, "0000");
+				pstmt.setString(i++, "가상계좌 등록이 완료되었습니다.");
+				pstmt.setString(i++, "SYSTEM");
+				pstmt.setString(i++, CommonUtil.getCurrentDate("yyyyMMdd"));
+				pstmt.addBatch();
+
+				if (++count % batchSize == 0) {
+					inserted += pstmt.executeBatch().length;
+				}
+			}
+			inserted += pstmt.executeBatch().length;
+			conn.commit();
+		} catch (Exception e) {
+			logger.debug("insert batch HT_VACT_DTL error : {}", CommonUtil.getExceptionMessage(e));
+		} finally {
+			db.close(pstmt);
+			db.close(conn);
+		}
+
+		logger.info("set HT_VACT_DTL insert : {}", inserted);
+
+		super.initRecord();
 		return inserted;
+	}
+
+	public SharedMap<String,Object> getVactDtl(String issueId) {
+		super.setTable("PG_VACT_DTL");
+		super.addWhere("issueId", issueId, eq);
+
+		super.setLimit(1);
+
+		RecordSet rset = super.search();
+		super.initRecord();
+		return rset.getRowFirst();
 	}
 }
 
