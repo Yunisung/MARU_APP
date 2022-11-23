72a83f0314b86436949ff20d621c153e52c8d97c
pys890102 <pys890102@bkwinners.com>
2022-11-23 13:01:26 +0900

    출금키 암호화 적용

--- src/com/pgmate/test/Test.java
+++ src/com/pgmate/test/Test.java
@@ -1,5 +1,8 @@
 package com.pgmate.test;
 
+import com.pgmate.app.util.KSignUtil;
+import com.pgmate.lib.key.CPKEY;
+import com.pgmate.lib.key.GenKey;
 import com.pgmate.lib.util.lang.CommonUtil;
 
 /**
@@ -16,7 +19,16 @@ public class Test {
 	}
 	
 	public static void main(String[] args){
-		System.out.println(CommonUtil.diffOfDay("20170401", "20170403", "yyyyMMdd"));
+//		System.out.println(CommonUtil.diffOfDay("20170401", "20170403", "yyyyMMdd"));
+//		String tk = GenKey.genKeys(CPKEY.CASH_TRANSFER, "winnerscom");
+//		System.out.println(tk);
+
+		ksignTest();
+	}
+
+	public static void ksignTest() {
+		String key = KSignUtil.getInstance().Encrypt("ct_8124-bc2e24-d90-e2c6a");
+		KSignUtil.getInstance().Decrypt(key);
 	}
 
 }
