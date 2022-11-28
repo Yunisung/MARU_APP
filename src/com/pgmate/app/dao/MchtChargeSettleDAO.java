7a6cb0a7e06d106bf5c2c67cef1470c6e0e772fb
yangwenry <yangwenry@bkwinners.com>
2022-11-28 15:59:53 +0900

    출금키 버튼으로 출금키 생성

--- src/com/pgmate/app/dao/MchtChargeSettleDAO.java
+++ src/com/pgmate/app/dao/MchtChargeSettleDAO.java
@@ -1,5 +1,7 @@
 package com.pgmate.app.dao;
 
+import com.pgmate.app.model.ajax.Data;
+import com.pgmate.lib.util.lang.CommonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +9,8 @@ import com.pgmate.app.util.CPUtil;
 import com.pgmate.lib.dao.DAO;
 import com.pgmate.lib.dao.RecordSet;
 import com.pgmate.lib.util.map.SharedMap;
+
+import java.util.List;
 
 /**
  * @author Administrator
@@ -35,5 +39,14 @@ public class MchtChargeSettleDAO extends DAO{
 		RecordSet rset = super.search();
 		super.initRecord();
 		return rset.getRowFirst();
+	}
+
+	public boolean updateTransferKey(String transferKey, String regId, String mchtId) {
+		this.setTable("PG_MCHT_CHARGE_MNG");
+		this.setRecord("transferKey", transferKey);
+		this.setRecord("regId", regId);
+		this.setRecord("regDay", CommonUtil.getCurrentDate("yyyyMMdd"));
+		addWhere("mchtId",mchtId.toLowerCase(),eq);
+		return super.update();
 	}
 }
