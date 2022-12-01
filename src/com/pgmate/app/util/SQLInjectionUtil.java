a92fb66177e604c045a63df59ad603e00f30c466
pys890102 <pys890102@bkwinners.com>
2022-12-01 12:55:21 +0900

    XSS 방지 테스트

--- src/com/pgmate/app/util/SQLInjectionUtil.java
+++ src/com/pgmate/app/util/SQLInjectionUtil.java
@@ -70,9 +70,8 @@ public class SQLInjectionUtil {
 		orgVal = evilChars.matcher(orgVal).replaceAll("");
 		
 		// 특수 문자 필터링
-		//210121 KJM : db 들어가는 json 값 치환 주석처리
-//		orgVal = orgVal.replaceAll("'", "&#39;");
-//		orgVal = orgVal.replaceAll("\"", "&#34;");
+		orgVal = orgVal.replaceAll("'", "&#39;");
+		orgVal = orgVal.replaceAll("\"", "&#34;");
 		// orgVal = orgVal.replaceAll("-", "&#45;");
 		// orgVal = orgVal.replaceAll("(", "&#40;");
 		// orgVal = orgVal.replaceAll(")", "&#41;");
