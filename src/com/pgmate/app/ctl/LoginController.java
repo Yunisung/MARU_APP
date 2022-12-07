89f506c2defa1c87db5f2a4756bf55f4cafba490
yangwenry <yangwenry@bkwinners.com>
2022-12-07 18:08:17 +0900

    spring-security 설정 - 코드정리2

--- src/com/pgmate/app/ctl/LoginController.java
+++ src/com/pgmate/app/ctl/LoginController.java
@@ -7,17 +7,13 @@ import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import com.pgmate.app.security.CustomUserDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +28,7 @@ import com.pgmate.app.dao.UserIpDAO;
 import com.pgmate.app.interceptor.SessionExclude;
 import com.pgmate.app.session.CPSession;
 import com.pgmate.app.util.CPUtil;
+import com.pgmate.app.util.InfoBankSMS;
 import com.pgmate.app.util.SessionUtil;
 import com.pgmate.app.util.WebCache;
 import com.pgmate.lib.dao.RecordSet;
@@ -39,6 +36,7 @@ import com.pgmate.lib.util.gson.GsonUtil;
 import com.pgmate.lib.util.lang.CommonUtil;
 import com.pgmate.lib.util.map.SharedMap;
 import com.pgmate.lib.sms.SmsUtil;
+
 
 /**
  * @author Administrator
