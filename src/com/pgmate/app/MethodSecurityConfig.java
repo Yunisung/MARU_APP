92e45ec654b14558073eda2122b8143e62fc30d6
yangwenry <yangwenry@bkwinners.com>
2022-12-07 18:08:17 +0900

    spring-security 설정

---
+++ src/com/pgmate/app/MethodSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.pgmate.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class MethodSecurityConfig {
+
+}
