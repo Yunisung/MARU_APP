3893f670998be558dda54e3f2e15c39301cc88df
pys890102 <pys890102@bkwinners.com>
2022-12-08 10:15:44 +0900

    공지사항 등록시 권한 체크

--- src/com/pgmate/app/ctl/NoticeController.java
+++ src/com/pgmate/app/ctl/NoticeController.java
@@ -52,7 +52,13 @@ public class NoticeController {
 
 	@RequestMapping(value = { "/system/notice/add" })
 	public ModelAndView add(HttpServletRequest request) {
-		return new ModelAndView("/system/notice/add");
+		CPSession session = SessionUtil.get(request);
+		if(!(session.getGrade().equals("본사") && !session.getRole().equals("일반"))) {
+			//관리자 권한이 없을때 이전 화면으로 보냄
+			return new ModelAndView("/");
+		} else {
+			return new ModelAndView("/system/notice/add");
+		}
 	}
 
 	@RequestMapping(value = "/system/notice/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
