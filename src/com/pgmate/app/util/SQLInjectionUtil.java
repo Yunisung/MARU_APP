1280a573b059a2265f10040400147ef43aa09841
pys890102 <pys890102@bkwinners.com>
2023-02-15 17:14:33 +0900

    SQLInjectionUtil 변경

--- src/com/pgmate/app/util/SQLInjectionUtil.java
+++ src/com/pgmate/app/util/SQLInjectionUtil.java
@@ -71,8 +71,8 @@ public class SQLInjectionUtil {
 		
 		// 특수 문자 필터링
 		orgVal = orgVal.replaceAll("'", "&#39;");
-		orgVal = orgVal.replaceAll("\"", "&#34;");
-		// orgVal = orgVal.replaceAll("-", "&#45;");
+//		orgVal = orgVal.replaceAll("\"", "&#34;");
+//		 orgVal = orgVal.replaceAll("-", "&#45;");
 		// orgVal = orgVal.replaceAll("(", "&#40;");
 		// orgVal = orgVal.replaceAll(")", "&#41;");
 		orgVal = orgVal.replaceAll("<", "&#60;");
@@ -88,4 +88,5 @@ public class SQLInjectionUtil {
 		}
 		return orgVal;
 	}
+
 }
