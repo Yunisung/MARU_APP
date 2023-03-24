a64c8765ae597415e3d61c70ea492256c5de7b74
kjm <jjggooo>
2023-03-24 11:01:11 +0900

    가맹점 터미널 이력정보 조회 쿼리 수정 2

--- src/com/pgmate/app/dao/MchtMngDAO.java
+++ src/com/pgmate/app/dao/MchtMngDAO.java
@@ -48,15 +48,15 @@ public class MchtMngDAO extends DAO{
 //		setColumns("*");
 //		addWhere("lower(mchtId)",mchtId.toLowerCase(),eq);
 //		return search();
-		super.setTable("(SELECT * FROM  HT_MCHT_TMN where mchtId ='" + mchtId + "'" +
-				"UNION ALL" +
+		super.setTable("(SELECT * FROM  HT_MCHT_TMN where mchtId ='" + mchtId + "' " +
+				"UNION ALL " +
 				"SELECT '' AS idx, tmnId, mchtId, taxId, status, serial, payKey, activeDate, apiMaxInstall" +
 				", webPay, appDirect, semiAuth" +
 				", refundType, van, vanIdx, ccType, description, minAmount, payLimit, limitAmount, limitStartTime" +
 				", limitEndTime, regId, regDay, regDate, '' as summary " +
-				"FROM PG_MCHT_TMN WHERE mchtId ='" + mchtId + "') AS HT_TMN" +
-				"LEFT OUTER JOIN PG_MCHT_TMN ON HT_TMN.tmnId = PG_MCHT_TMN.tmnId");
-		super.setColumns("HT_TMN.*, PG_MCHT_TMN.tmnId AS originId, PG_MCHT_TMN.regDate AS activeDate");
+				"FROM PG_MCHT_TMN WHERE mchtId ='" + mchtId + "') AS HT_TMN " +
+				"LEFT OUTER JOIN PG_MCHT_TMN ON HT_TMN.tmnId = PG_MCHT_TMN.tmnId ");
+		super.setColumns("HT_TMN.*, PG_MCHT_TMN.tmnId AS originId, PG_MCHT_TMN.regDate AS activeDate ");
 		super.setOrderBy("PG_MCHT_TMN.regDate DESC, PG_MCHT_TMN.tmnId DESC, HT_TMN.regDate DESC ");
 		return super.search();
 	}
