8095601c97584e448efb132629be4876622aed3b
pys890102 <pys890102@bkwinners.com>
2023-03-28 17:30:52 +0900

    정기결제 키 추가

--- src/com/pgmate/lib/key/CPKEY.java
+++ src/com/pgmate/lib/key/CPKEY.java
@@ -164,6 +164,10 @@ public enum CPKEY {
 	CASH_TRANSFER{
 		@Override
 		public String toString() { return "ct_"; }
+	},
+	REBILL {
+		@Override
+		public String toString() { return "rb_"; }
 	}
 	
 	
