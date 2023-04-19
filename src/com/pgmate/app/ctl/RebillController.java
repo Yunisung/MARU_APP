c6d1843b8b339130b0e0c5a83cb201b5777ebc1a
kjm <jjggooo>
2023-04-19 15:31:00 +0900

    정기결제 - 거래내역, 오류내역 추가

--- src/com/pgmate/app/ctl/RebillController.java
+++ src/com/pgmate/app/ctl/RebillController.java
@@ -1,8 +1,6 @@
 package com.pgmate.app.ctl;
 
-import com.pgmate.app.dao.MchtTmnDAO;
-import com.pgmate.app.dao.RebillDAO;
-import com.pgmate.app.dao.VactDtlDAO;
+import com.pgmate.app.dao.*;
 import com.pgmate.app.model.ajax.CPRequest;
 import com.pgmate.app.session.CPSession;
 import com.pgmate.app.util.CPRUtil;
@@ -13,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,5 +40,36 @@ public class RebillController {
         RebillDAO rebillRegDAO = new RebillDAO();
         RecordSet rset = rebillRegDAO.regList(cpRequest.data, cpRequest.page);
         return new CPRUtil(cpRequest).dataList(rset, rebillRegDAO).setView(request, "/rebill/reg/list", "");
+    }
+
+    @RequestMapping(value = "/rebill/trx/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView trxList(HttpServletRequest request, @RequestBody CPRequest cpRequest) {
+        SessionUtil.setSearchGrade(request, cpRequest);
+
+        RebillDAO rebillTrxDAO = new RebillDAO();
+        RecordSet rset = rebillTrxDAO.trxList(cpRequest.data, cpRequest.page);
+        request.setAttribute("AMOUNT_SUM", rebillTrxDAO.trxSum(cpRequest.data,null).getRowFirst().getString("amount"));
+        return new CPRUtil(cpRequest).dataList(rset, rebillTrxDAO).setView(request, "/rebill/trx/list", "");
+    }
+
+    @RequestMapping(value = "/rebill/trx/view/{trxId}", method = RequestMethod.GET)
+    public ModelAndView payView(HttpServletRequest request, @PathVariable String trxId) {
+        request.setAttribute("DATAMAP", new RebillDAO().getByTrxId(trxId).getRow(0));
+        return new ModelAndView("/rebill/trx/modal");
+    }
+
+    @RequestMapping(value = "/rebill/err/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView errList(HttpServletRequest request, @RequestBody CPRequest cpRequest) {
+        SessionUtil.setSearchGrade(request, cpRequest);
+
+        RebillDAO rebillTrxDAO = new RebillDAO();
+        RecordSet rset = rebillTrxDAO.errList(cpRequest.data, cpRequest.page);
+        return new CPRUtil(cpRequest).dataList(rset, rebillTrxDAO).setView(request, "/rebill/err/list", "");
+    }
+
+    @RequestMapping(value = "/rebill/err/view/{trxId}", method = RequestMethod.GET)
+    public ModelAndView errView(HttpServletRequest request, @PathVariable String trxId) {
+        request.setAttribute("DATAMAP", new RebillDAO().getByErrId(trxId).getRow(0));
+        return new ModelAndView("/rebill/err/modal");
     }
 }
