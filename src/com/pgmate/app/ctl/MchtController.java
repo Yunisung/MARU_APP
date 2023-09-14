395d7a9aa66c42631a8f23ac7b23ecc090d13ed8
kjm <jjggooo>
2023-09-14 09:59:13 +0900

    월세앱 탭 추가 2차

--- src/com/pgmate/app/ctl/MchtController.java
+++ src/com/pgmate/app/ctl/MchtController.java
@@ -3227,4 +3227,49 @@ public class MchtController {
 
 	}
 
+	@RequestMapping(value = {"/mcht/rent/add/{mchtId}"})
+	public ModelAndView rentAdd(HttpServletRequest request, @PathVariable String mchtId) {
+		SharedMap<String,Object> result = new MchtDAO().getById(mchtId).getRowFirst();
+
+		request.setAttribute("DATAMAP", result);
+
+		return new ModelAndView("/mcht/rent/add", "MCHT_MAP", result);
+	}
+
+	@RequestMapping(value = {"/mcht/rent/insert"}, method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody CPResponse rentInsert(HttpServletRequest request, @RequestBody CPRequest cpRequest) {
+		CPDAO cpDAO = new CPDAO();
+
+		if(cpDAO.insertByOper("PG_MCHT_RENT", SessionUtil.getUserId(request), cpRequest.data)){
+			SessionUtil.initSessionData(request);
+			return new CPRUtil(cpRequest)
+					.resultOK(CPUtil.RESULT_DATA_INSERTED).redirect(cpRequest.redirect)
+					.cpResponse();
+		}else{
+			return new CPRUtil(cpRequest)
+					.resultNOK(CPUtil.RESULT_DATA_INFAIL,cpDAO.getError())
+					.cpResponse();
+		}
+	}
+	@RequestMapping(value = "/mcht/rent/modify/{mchtId}", method = RequestMethod.GET)
+	public ModelAndView rentModify(HttpServletRequest request, @PathVariable String mchtId) {
+		SharedMap<String,Object> result = new MchtDAO().getById(mchtId).getRowFirst();
+		SharedMap<String,Object> rentMap = new MchtRentDAO().getByMchtId(mchtId);
+		request.setAttribute("DATAMAP", result);
+		request.setAttribute("RENTMAP", rentMap);
+
+		return new ModelAndView("/mcht/rent/modify","MCHT_MAP",result);
+	}
+
+	@RequestMapping(value = {"/mcht/rent/update"}, method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody CPResponse rentUpdate(HttpServletRequest request, @RequestBody CPRequest cpRequest) {
+		CPDAO cpDAO = new CPDAO();
+		if (cpDAO.updateAndBackByOper("PG_MCHT_RENT", SessionUtil.getUserId(request), cpRequest.data)) {
+			return new CPRUtil(cpRequest).resultOK("정기결제 정보가 변경되었습니다.").cpResponse();
+		} else {
+			return new CPRUtil(cpRequest).resultNOK("정기결제 정보 변경에 실패하였습니다.",cpDAO.getError())
+					.cpResponse();
+		}
+
+	}
 }
