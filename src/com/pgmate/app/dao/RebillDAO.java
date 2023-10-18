6018e851882371db799bdebed86acc3d0320794c
pys890102 <pys890102@bkwinners.com>
2023-10-18 16:13:05 +0900

    281019_정기결제 머지
    # Conflicts:
    #	src/com/pgmate/app/dao/RebillDAO.java

--- src/com/pgmate/app/dao/RebillDAO.java
+++ src/com/pgmate/app/dao/RebillDAO.java
@@ -19,6 +19,34 @@ public class RebillDAO extends DAO {
         return super.searchList(page.current, page.size, page.hash); //LIST PAGING
     }
 
+    public RecordSet trxList(List<Data> datas, Page page) {
+        super.setTable("PG_REBILL_PAY");
+
+        page = CPUtil.correctPage(page);
+        CPUtil.setDAO(this, datas);
+        return super.searchList(page.current, page.size, page.hash);
+    }
+
+    public RecordSet trxSum(List<Data> datas,Page page) {
+        //KJM : 금액의 합계를 amount 컬럼명으로 받겠다
+        super.setColumns("SUM(amount) AS amount");
+        super.addWhere("status", "승인");
+        page = CPUtil.correctPage(page);
+        CPUtil.setDAO(this, datas);				//DATA to CONDITION
+        RecordSet rset =  super.search();
+        super.initRecord();
+        return rset;	//LIST PAGING 검색
+    }
+
+    public RecordSet getByTrxId(String trxId) {
+        super.setTable("VW_REBILL_PAY");
+        super.setColumns("*");
+        super.addWhere("trxId", trxId);
+        RecordSet rset = super.search();
+        super.initRecord();
+        return rset;
+    }
+
     public RecordSet errList(List<Data> datas, Page page) {
         super.setTable("PG_REBILL_ERR");
 
