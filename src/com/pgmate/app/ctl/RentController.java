f4a24a774b1c55c375cc40f554d9afc2a7c50de0
kjm <jjggooo>
2023-11-28 13:22:07 +0900

    월세앱 - 화면구성

---
+++ src/com/pgmate/app/ctl/RentController.java
@@ -0,0 +1,346 @@
+package com.pgmate.app.ctl;
+
+import com.pgmate.app.dao.*;
+import com.pgmate.app.model.ajax.CPRequest;
+import com.pgmate.app.util.CPRUtil;
+import com.pgmate.app.util.SessionUtil;
+import com.pgmate.lib.dao.RecordSet;
+import com.pgmate.lib.util.lang.CommonUtil;
+import com.pgmate.lib.util.map.SharedMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RentController {
+
+    private static Logger logger = LoggerFactory.getLogger( com.pgmate.app.ctl.RentController.class );
+
+    // 가맹점 조회
+    @RequestMapping(value = {"/rent/mcht/form"})
+    public ModelAndView mchtForm(HttpServletRequest request) {
+        return new ModelAndView("/rent/mcht/form");
+    }
+
+    @RequestMapping(value = "/rent/mcht/list", method = RequestMethod.POST,produces= MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView mchtList(HttpServletRequest request, HttpServletResponse response, @RequestBody CPRequest cpRequest) {
+        RentDAO rentDAO = new RentDAO();
+        SessionUtil.setSearchGrade(request, cpRequest);
+        cpRequest.replaceKeyValue("identity",rentDAO.getAESEnc(cpRequest.getKeyValue("identity")));
+        cpRequest.replaceKeyValue("ceoIdentity",rentDAO.getAESEnc(cpRequest.getKeyValue("ceoIdentity")));
+        RecordSet rset = rentDAO.exList(cpRequest.data,cpRequest.page);
+        return new CPRUtil(cpRequest).dataList(rset,rentDAO).setView(request,"/rent/mcht/list","");
+    }
+
+    // 매입현황조회
+    @RequestMapping(value = {"/rent/cap/form"})
+    public ModelAndView capForm(HttpServletRequest request) {
+        return new ModelAndView("/rent/cap/form");
+    }
+
+   @RequestMapping(value = "/rent/cap/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView capList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
+        RentDAO rentDAO = new RentDAO();
+        SessionUtil.setSearchGrade(request, cpRequest);
+
+        cpRequest.replaceKeyName("amount", "abs(amount)");
+
+        request.setAttribute("AMOUNT_SUM", rentDAO.trxSum(cpRequest.data,null).getRowFirst().getString("amount"));
+
+        RecordSet rset = rentDAO.list(cpRequest.data,cpRequest.page);
+        return new CPRUtil(cpRequest).dataList(rset,rentDAO).setView(request,"/rent/cap/list","");
+    }
+
+    @RequestMapping(value = "/rent/cap/view/{capId}", method = RequestMethod.GET)
+    public ModelAndView capView(HttpServletRequest request, @PathVariable String capId) {
+        SharedMap<String, Object> res =  new TrxCapDAO().getByCapId(capId).getRow(0);
+
+        //PYS : 갤럭시아 영수증 조회용
+        if(res.startsWith("van", "GALAXIA")) {
+            res.put("GalaxiaMID", res.getString("vanId"));
+        }
+
+        request.setAttribute("DATAMAP", res);
+        request.setAttribute("DATAREFMAP", new TrxCapDAO().getByRootTrxId(res.getString("trxId")).getRow(0));
+
+        request.setAttribute("DATATMNMAP", new MchtTmnDAO().getById(res.getString("tmnId")).getRow(0));
+
+        request.setAttribute("IQR_MAP", new TrxIqrDAO().getByCapId(capId).getRows());
+        return new ModelAndView("/rent/cap/modal");
+    }
+
+    // 거래내역
+    @RequestMapping(value = {"/rent/trx/form"})
+    public ModelAndView trxForm(HttpServletRequest request) {
+        return new ModelAndView("/rent/trx/form");
+    }
+
+    @RequestMapping(value = "/rent/trx/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView trxList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
+        SessionUtil.setSearchGrade(request, cpRequest);
+        RentDAO rentDAO = new RentDAO();
+
+        request.setAttribute("SUMMAP", rentDAO.depositSum(cpRequest.data).getRow(0));
+
+        RecordSet rset = rentDAO.listWithDecAccount(cpRequest.data,cpRequest.page);
+        return new CPRUtil(cpRequest).dataList(rset,rentDAO).setView(request,"/rent/trx/list","");
+    }
+
+    @RequestMapping(value = "/rent/trx/view/{trxId}", method = RequestMethod.GET)
+    public ModelAndView trxView(HttpServletRequest request, @PathVariable("trxId") String trxId) {
+        request.setAttribute("DATAMAP", new ChargeSettleDAO().getById(trxId).getRow(0));
+        request.setAttribute("DATANOTIMAP", new ChargeSettleNotiDAO().getById(trxId).getRow(0));
+        return new ModelAndView("/rent/trx/modal");
+    }
+
+    @RequestMapping(value = "/rent/trx/transfer/view/{trxId}", method = RequestMethod.GET)
+    public ModelAndView transferView(HttpServletRequest request, @PathVariable("trxId") String trxIdList) {
+        // 거래아이디 리스트 중 마지막 ',' 제거
+        trxIdList = trxIdList.substring(0, trxIdList.length() - 1);
+
+        String[] trxIdArray = CommonUtil.split(trxIdList, ",",true);
+        RentDAO rentDAO = new RentDAO();
+
+        request.setAttribute("DATAMAP", new ChargeSettleDAO().getById("T231116043912").getRow(0));
+        request.setAttribute("DATANOTIMAP", new ChargeSettleNotiDAO().getById("T231116043912").getRow(0));
+        request.setAttribute("SUMAMT", rentDAO.getSumAmt(trxIdList));
+        request.setAttribute("TRXIDLIST", trxIdList);
+        return new ModelAndView("/rent/trx/transferModal");
+    }
+
+    @RequestMapping(value = "/rent/trx/transfer/{trxId}", method = RequestMethod.POST)
+    public @ResponseBody String transfer(HttpServletRequest request, @PathVariable("trxId") String trxIdList) {
+        String[] trxIdArray = CommonUtil.split(trxIdList, ",",true);
+        logger.info("trxId : {}", trxIdArray[0]);
+        RentDAO rentDAO = new RentDAO();
+
+        String transferType = CommonUtil.nToB(request.getParameter("transferType"));
+        String pubDay = "";
+        String pubTime = "";
+
+        String toDay = CommonUtil.getCurrentDate("yyyyMMdd");
+
+        if(transferType.equals("예약")) {
+            pubTime = CommonUtil.nToB(request.getParameter("pubTime"));
+            pubDay = CommonUtil.nToB(request.getParameter("pubDay"));
+
+            if(Integer.valueOf(pubDay) <= Integer.valueOf(toDay)) {
+                return "NOK:이체예정일자는 익일부터 가능합니다.";
+            }
+        }
+
+        Long amount = Long.valueOf(CommonUtil.nToB(request.getParameter("amount")));
+
+        logger.info("data :: {} : {} : {}", pubDay, pubTime, amount);
+
+        // 분납 거래건 valid
+        List<SharedMap<String,Object>> sharedMapList = null;
+
+        // 이체 처리 된 거래건인지
+//        sharedMapList = rentDAO.isCSReserve(trxIdList);
+//        if(sharedMapList.size() > 0) {
+//            sharedMapList.clear();
+//            return "NOK:이체처리된 거래건입니다.";
+//        }
+
+        // 같은 가맹점인지
+        sharedMapList = rentDAO.isSingleMchtId(trxIdList);
+        if(sharedMapList.size() > 0) {
+            String firstMchtId = sharedMapList.get(0).getString("mchtId");
+            for(SharedMap<String,Object> map : sharedMapList) {
+                if(!map.getString("mchtId").equals(firstMchtId)) {
+                    return "NOK:같은 가맹점 아님";
+                }
+            }
+            sharedMapList.clear();
+        }
+
+        // 같은 계좌인지
+        sharedMapList = rentDAO.isSameAccount(trxIdList);
+        if(sharedMapList.size() > 0) {
+            String firstAccount = sharedMapList.get(0).getString("account");
+            for(SharedMap<String,Object> map : sharedMapList) {
+                if(!map.getString("account").equals(firstAccount)) {
+                    return "NOK:같은 계좌번호 아님";
+                }
+            }
+            sharedMapList.clear();
+        }
+
+        // 리스크 거래건인지
+        sharedMapList = rentDAO.isRiskTrx(trxIdList);
+        if(sharedMapList.size() > 0) {
+            for(SharedMap<String,Object> map : sharedMapList) {
+                if(!map.getString("risk").equals("")) {
+                    return "NOK:리스크 거래건";
+                }
+            }
+            sharedMapList.clear();
+        }
+
+        SharedMap<String, Object> capMap;
+        SharedMap<String, Object> mchtTaxMap;
+        List<SharedMap<String, Object>> insertMapList = new ArrayList<>();
+
+        // 분납 합산건 생성
+        capMap = rentDAO.getCapMapByTrxId(trxIdArray[0].replaceAll("'",""));
+        mchtTaxMap = rentDAO.getTaxMapByMchtId(capMap.getString("mchtId"));
+        SharedMap<String, Object> newMap = setInsertNewMap(trxIdList,transferType, pubDay, pubTime, amount, capMap, mchtTaxMap);
+        insertMapList.add(newMap);
+        logger.info("======>{}",newMap.getString("trxId"));
+
+        capMap.clear();
+        mchtTaxMap.clear();
+
+        // 분납 개별건 처리
+        for(String trxId : trxIdArray) {
+            trxId = trxId.replaceAll("'", "");
+            if(!trxId.equals("")){
+                capMap = rentDAO.getCapMapByTrxId(trxId);
+                mchtTaxMap = rentDAO.getTaxMapByMchtId(capMap.getString("mchtId"));
+                insertMapList.add(setInsertMap(transferType, pubDay, pubTime, newMap.getString("trxId"), capMap, mchtTaxMap));
+            }
+        }
+
+        rentDAO.insertFirmReserve(insertMapList);
+
+        return "OK";
+    }
+
+    public SharedMap<String, Object> setInsertNewMap(String trxIdList, String transferType, String pubDay, String pubTime, Long amount, SharedMap<String, Object> capMap, SharedMap<String, Object> mchtTaxMap) {
+        SharedMap<String, Object> map = new SharedMap<>();
+        RentDAO rentDAO = new RentDAO();
+        // reserve
+        // trxId, transferType, mchtId, trackId, pubDay, pubTime, status, retry, trxDay, trxTime, amount, fee, feeVat, bankFee, netAmount,
+        // balance, resultCd, resultMsg, refId, rootTrxId, account, bankCd, bankName, holder, recordInfo, regId, regDay, regDate
+        String trxId = rentDAO.getTrxId();
+        map.put("trxId", trxId);
+        map.put("transferType"	, transferType);
+        map.put("mchtId"	, capMap.getString("mchtId"));
+        map.put("trackId"	, "rent-" + CommonUtil.getCurrentDate("yyyyMMddhhmmss"));
+        map.put("pubDay"	, pubDay);
+        map.put("pubTime"	, pubTime);
+        map.put("status"	, "대기");
+        map.put("retry"		, 0);
+        map.put("trxDay"	, CommonUtil.getCurrentDate("yyyyMMdd"));
+        map.put("trxTime"	, CommonUtil.getCurrentDate("hhmmss"));
+        map.put("amount"	, Math.abs(amount));
+        map.put("fee"		, Math.abs(rentDAO.getFeeSum(trxIdList)));
+        map.put("feeVat"	, Math.abs(rentDAO.getFeeVatSum(trxIdList)));
+        map.put("bankFee"	, 0);
+        map.put("netAmount"	, Math.abs(capMap.getLong("stlAmount")));
+        map.put("netAmount"	, Math.abs(rentDAO.getNetAmountSum(trxIdList)));
+        map.put("balance"	, rentDAO.getMchtBalance(capMap.getString("mchtId")).getLong("balance")+Math.abs(capMap.getLong("stlAmount")));
+        map.put("resultCd"	, "");
+        map.put("resultMsg"	, "");
+        map.put("refId"		, trxId);
+        map.put("rootTrxId"	, "");
+        map.put("account"	, rentDAO.getAESEnc(mchtTaxMap.getString("account")));
+        map.put("bankCd"	, mchtTaxMap.getString("bankCd"));
+        map.put("bankName"	, mchtTaxMap.getString("bankName"));
+        map.put("holder"	, rentDAO.getAESEnc(mchtTaxMap.getString("accntHolder")));
+        map.put("recordInfo", rentDAO.getSender(capMap.getString("mchtId")));
+        map.put("regId"		, capMap.getString("mchtId"));
+        map.put("regDay"	, CommonUtil.getCurrentDate("yyyyMMdd"));
+
+        return map;
+    }
+
+    public SharedMap<String, Object> setInsertMap(String transferType, String pubDay, String pubTime, String rootTrxId, SharedMap<String, Object> capMap, SharedMap<String, Object> mchtTaxMap) {
+        SharedMap<String, Object> map = new SharedMap<>();
+        RentDAO rentDAO = new RentDAO();
+        // reserve
+        // trxId, transferType, mchtId, trackId, pubDay, pubTime, status, retry, trxDay, trxTime, amount, fee, feeVat, bankFee, netAmount,
+        // balance, resultCd, resultMsg, refId, rootTrxId, account, bankCd, bankName, holder, recordInfo, regId, regDay, regDate
+        map.put("trxId", capMap.getString("trxId"));
+        map.put("transferType"	, transferType);
+        map.put("mchtId"	, capMap.getString("mchtId"));
+        map.put("trackId"	, capMap.getString("trackId"));
+        map.put("pubDay"	, pubDay);
+        map.put("pubTime"	, pubTime);
+        map.put("status"	, "대기");
+        map.put("retry"		, 0);
+        map.put("trxDay"	, capMap.getString("trxDay"));
+        map.put("trxTime"	, capMap.getString("regTime"));
+        map.put("amount"	, Math.abs(capMap.getLong("amount")));
+        map.put("fee"		, Math.abs(capMap.getLong("stlFee")));
+        map.put("feeVat"	, Math.abs(capMap.getLong("stlFeeVat")));
+        map.put("bankFee"	, 0);
+        map.put("netAmount"	, Math.abs(capMap.getLong("stlAmount")));
+        map.put("balance"	, rentDAO.getMchtBalance(capMap.getString("mchtId")).getLong("balance")+Math.abs(capMap.getLong("stlAmount")));
+        map.put("resultCd"	, "");
+        map.put("resultMsg"	, "");
+        map.put("refId"		, capMap.getString("capId"));
+        map.put("rootTrxId"	, rootTrxId);
+        map.put("account"	, rentDAO.getAESEnc(mchtTaxMap.getString("account")));
+        map.put("bankCd"	, mchtTaxMap.getString("bankCd"));
+        map.put("bankName"	, mchtTaxMap.getString("bankName"));
+        map.put("holder"	, rentDAO.getAESEnc(mchtTaxMap.getString("accntHolder")));
+        map.put("recordInfo", rentDAO.getSender(capMap.getString("mchtId")));
+        map.put("regId"		, capMap.getString("mchtId"));
+        map.put("regDay"	, CommonUtil.getCurrentDate("yyyyMMdd"));
+
+        return map;
+    }
+
+    // 거래내역
+    @RequestMapping(value = {"/rent/settle/form"})
+    public ModelAndView settleForm(HttpServletRequest request) {
+        return new ModelAndView("/rent/settle/form");
+    }
+
+    @RequestMapping(value = "/rent/settle/list", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView settleList(HttpServletRequest request,@RequestBody CPRequest cpRequest) {
+        SessionUtil.setSearchGrade(request, cpRequest);
+        RentDAO rentDAO = new RentDAO();
+
+        request.setAttribute("SUMMAP", rentDAO.getReserveSum(cpRequest.data, null).getRow(0));
+
+        RecordSet rset = rentDAO.getRentSettleList(cpRequest.data,cpRequest.page);
+        return new CPRUtil(cpRequest).dataList(rset,rentDAO).setView(request,"/rent/settle/list","");
+    }
+
+    @RequestMapping(value = "/rent/settle/retry/view/{trxId}", method = RequestMethod.GET)
+    public ModelAndView settleRetryView(HttpServletRequest request, @PathVariable("trxId") String trxId) {
+        RentDAO rentDAO = new RentDAO();
+
+        request.setAttribute("TRXID", trxId);
+        request.setAttribute("AMOUNT", rentDAO.getAmount(trxId));
+        return new ModelAndView("/rent/settle/transferModal");
+    }
+
+    @RequestMapping(value = "/rent/settle/retry/{trxId}", method = RequestMethod.POST)
+    public @ResponseBody String retry(HttpServletRequest request, @PathVariable("trxId") String trxId) {
+        RentDAO rentDAO = new RentDAO();
+
+        String transferType = CommonUtil.nToB(request.getParameter("transferType"));
+        String pubDay = "";
+        String pubTime = "";
+
+        String toDay = CommonUtil.getCurrentDate("yyyyMMdd");
+
+        if(transferType.equals("예약")) {
+            pubTime = CommonUtil.nToB(request.getParameter("pubTime"));
+            pubDay = CommonUtil.nToB(request.getParameter("pubDay"));
+
+            if(Integer.valueOf(pubDay) <= Integer.valueOf(toDay)) {
+                return "NOK:이체예정일자는 익일부터 가능합니다.";
+            }
+        }
+
+
+        if(rentDAO.updateFirmReserve(trxId, transferType, pubDay, pubTime)) {
+            return "OK:재전송 요청 성공하였습니다.";
+        } else {
+            return "NOK:재전송 요청에 실패하였습니다.";
+        }
+    }
+}
---
+++ web/rent/settle/modal.jsp
@@ -0,0 +1,325 @@
+<%@page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt_rt"%>
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head></head>
+<body>
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+		<h4>${DATAMAP.trxId} 거래 내역 상세정보</h4>
+	</div>
+	<div class="modal-body">
+		<ul class="nav nav-tabs">
+			<li class="active">
+				<a href="#tab1" data-toggle="tab">거래정보</a>
+			</li>
+			<c:if test="${CP_SESSION.grade == '본사' && DATAMAP.trxType eq '출금' && DATAMAP.trxUnit eq '펌뱅킹'}">
+				<li>
+					<a href="#tab_hook" data-toggle="tab">통지 정보</a>
+				</li>
+			</c:if>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane active" id="tab1">	
+				<!-- BEGIN FORM-->
+				<form class="form-horizontal form" role="form">
+					<div class="form-body row">
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">거래번호</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.trxId}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">가맹점명</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.name}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">가맹점아이디</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.mchtId}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">거래구분</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.trxType}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">거래유형</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.trxUnit}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">입출금 원금</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.amount}</p>
+								</div>
+							</div>
+						</div>
+						<c:if test="${DATAMAP.trxType eq '출금'}">
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">수수료</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.fee}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">수수료부가세</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.feeVat}</p>
+								</div>
+							</div>
+						</div>
+						</c:if>
+						<c:if test="${CP_SESSION.grade eq '본사' && DATAMAP.trxType eq '출금'}">
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">은행수수료</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.bankFee}</p>
+								</div>
+							</div>
+						</div>
+						</c:if>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">계정 실출금액</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.netAmount}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">거래 후 잔액</label>
+								<div class="col-md-9">
+									<p class="form-control-static digits">${DATAMAP.balance}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">주문번호</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.trackId}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">참조번호</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.refId}</p>
+								</div>
+							</div>
+						</div>
+						<c:if test="${DATAMAP.trxType eq '출금'}">
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">은행코드</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.bankCd}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">은행이름</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.bankName}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">계좌번호</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.account}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">받는계좌 예금주명</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.holder}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">적요</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.recordInfo}</p>
+								</div>
+							</div>
+						</div>
+						</c:if>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">등록자</label>
+								<div class="col-md-9">
+									<p class="form-control-static">${DATAMAP.regId}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class="col-md-6">
+							<div class="form-group pg-view-group">
+								<label class="control-label col-md-3">거래일시</label>
+								<div class="col-md-9">
+									<p class="form-control-static date">${DATAMAP.trxDay}${DATAMAP.trxTime}</p>
+								</div>
+							</div>
+						</div>
+						<!--/span-->
+						<div class='col-md-6'>
+							<div class='form-group pg-view-group'>
+								<label class='control-label col-md-3'>기재내용</label>
+								<div class='col-md-9'>
+									<p class='form-control-static'>${DATAMAP.summary}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+				<!-- END FORM-->
+			</div>
+			<div class="tab-pane" id="tab_hook">
+				<!-- BEGIN FORM-->
+				<div class="form-body row">
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'>Status</label>
+							<div class='col-md-8'>
+								<p class='form-control-static'>${DATANOTIMAP.status}</p>
+							</div>
+						</div>
+					</div>
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'>Retry</label>
+							<div class='col-md-8'>
+								<p class='form-control-static'>${DATANOTIMAP.retry}</p>
+							</div>
+						</div>
+					</div>
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'>Type</label>
+							<div class='col-md-8'>
+								<p class='form-control-static'>${DATANOTIMAP.hookType}</p>
+							</div>
+						</div>
+					</div>
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'>Address</label>
+							<div class='col-md-8'>
+								<p class='form-control-static'>${DATANOTIMAP.hookAddr}</p>
+							</div>
+						</div>
+					</div>
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'>Sent Date</label>
+							<div class='col-md-8'>
+								<p class='form-control-static'>${DATANOTIMAP.sentDate}</p>
+							</div>
+						</div>
+					</div>
+					<!--/span-->
+					<div class='col-md-4'>
+						<div class='form-group pg-view-group'>
+							<label class='control-label col-md-4'></label>
+							<div class='col-md-8'>
+							<c:if test="${!empty DATANOTIMAP }">
+								<button class="btn btn-sm blue" onClick="retryHook();">재전송</button>
+							</c:if>
+							<c:if test="${empty DATANOTIMAP }">
+								<p class='form-control-static'>전송중</p>
+							</c:if>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal-footer">
+		<button type="button" data-dismiss="modal" class="btn btn-sm">Close</button>
+	</div>
+	<script>
+		function retryHook() {
+			bootbox.confirm('해당 거래를 재전송 하시겠습니까?', function(result) {
+				if (result) {
+					$.ajax({
+						type: "GET",
+						url: "/chargeSettle/retry/${DATAMAP.trxId}",
+						success: function (res) {
+							if(res.result == 'OK') {
+								bootbox.alert('재전송 요청에 성공했습니다.');
+							} else {
+								bootbox.alert(res.msg);
+							}
+						},
+						error: function (res, status) {
+							bootbox.alert('재전송 요청에 실패했습니다.' + status);
+						}
+					});
+				}
+			});
+		};
+	</script>
+</body>
+</html>
