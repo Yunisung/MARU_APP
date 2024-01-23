8c8458794cc5ccadbfceac0635fc875a41a6c8b5
kjm <jjggooo>
2024-01-23 18:33:21 +0900

    월세앱 가맹점정산 - 검색조건 수정

--- src/com/pgmate/app/dao/RentDAO.java
+++ src/com/pgmate/app/dao/RentDAO.java
@@ -331,7 +331,8 @@ public class RentDAO extends DAO {
 
     public RecordSet getReserveSum(List<Data> datas,Page page) {
         super.setDebug(true);
-        super.setTable("PG_CHARGE_SETTLE_FIRM_RESERVE");
+//        super.setTable("PG_CHARGE_SETTLE_FIRM_RESERVE");
+        super.setTable("(SELECT A.*, B.regDay as payDay FROM PG_CHARGE_SETTLE_FIRM_RESERVE A LEFT OUTER JOIN VW_TRX_CAP B ON A.refTrxId=B.trxId) AS C");
         super.setColumns("SUM(amount) AS amount");
         page = CPUtil.correctPage(page);
         CPUtil.setDAO(this, datas);				//DATA to CONDITION
@@ -342,7 +343,7 @@ public class RentDAO extends DAO {
 
     public RecordSet getRentSettleList(List<Data> data, Page page) {
         super.setDebug(true);
-        super.setTable("(SELECT A.*, FN_AES_DEC(A.account) as decAccount, FN_AES_DEC(A.holder) as decHolder, C.name, B.billingType, B.billingMethod, B.regDay as payDay, B.regTime as payTime FROM PG_CHARGE_SETTLE_FIRM_RESERVE A LEFT OUTER JOIN VW_TRX_CAP B ON A.trxId=B.trxId LEFT OUTER JOIN PG_MCHT C ON A.mchtId=C.mchtId) AS D");
+        super.setTable("(SELECT A.*, FN_AES_DEC(A.account) as decAccount, FN_AES_DEC(A.holder) as decHolder, C.name, B.billingType, B.billingMethod, B.regDay as payDay, B.regTime as payTime FROM PG_CHARGE_SETTLE_FIRM_RESERVE A LEFT OUTER JOIN VW_TRX_CAP B ON A.refTrxId=B.trxId LEFT OUTER JOIN PG_MCHT C ON A.mchtId=C.mchtId) AS D");
         super.setColumns("*");
         page = CPUtil.correctPage(page);
         CPUtil.setDAO(this, data);				//DATA to CONDITION
