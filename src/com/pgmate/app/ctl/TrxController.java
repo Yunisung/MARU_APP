263b90f76f4721961a3fa1ac97e023b8707873f8
pys890102 <pys890102@bkwinners.com>
2024-06-13 16:17:34 +0900

    240613_웰컴, 웰컴서브 영수증 조회 기능 추가

--- src/com/pgmate/app/ctl/TrxController.java
+++ src/com/pgmate/app/ctl/TrxController.java
@@ -1103,7 +1103,6 @@ public class TrxController {
 			  }
 
 			  try {
-				  
 				  SharedMap<String, Object> rfd = new TrxCapDAO().getByTrxIdRfd(trxId).getRow(0);
 				  // 취소 원짱의 trxId가 입력될 경우 원거래 번호를 담는다.
 				  if(rfd != null) {
@@ -1127,6 +1126,59 @@ public class TrxController {
 					  }
 
 				  }
+
+				  //웰컴 서브 영수증 조회용
+				  if(res.startsWith("van", "WELCOMESUB")){
+					  SharedMap<String, Object> van  = new TrxCapDAO().getByVanId(res.getString("vanId")).getRow(0);
+
+					  try {
+						  res.put("hash_value", EncryptUtil.sha256(res.getString("vanTrxId") + van.getString("cryptoKey")));
+					  } catch (Exception e) {
+						  e.printStackTrace();
+					  }
+
+
+					  //온라인거래일때만, 거래정보 조회해서 영수증 ID 받기
+					  if(van.getString("vanId").equals("welcome306")) {
+						  String search_uri = "https://payapi.welcomepayments.co.kr/api/search/order";
+
+						  String mid = van.getString("vanId");
+						  String order_no = res.getString("trxId");
+
+						  String api_key = "59a30bd3e66d9a87c71b5d39e26ed42a";
+
+						  SharedMap<String, Object> reqMap = new SharedMap<>();
+						  reqMap.put("mid", mid);
+						  reqMap.put("order_no", order_no);
+
+						  String hash_value = "";
+						  try {
+							  hash_value = EncryptUtil.sha256(mid + order_no + api_key);
+							  reqMap.put("hash_value", hash_value);
+						  } catch (Exception e) {
+							  logger.error("WELCOME 영수증 조회 오류 : {}", e.getMessage());
+						  }
+
+						  SharedMap<String, Object> responseMap = new SharedMap<>();
+
+						  //통신
+						  responseMap = searchRequest(search_uri, reqMap.toJson());
+
+						  //결과값 처리
+						  if(responseMap.getString("result_code").equals("0000")) {
+							  String transaction_no = responseMap.getString("transaction_no");
+							  res.put("vanTrxId", transaction_no);
+
+							  try {
+								  res.put("hash_value", EncryptUtil.sha256(res.getString("vanTrxId") + api_key));
+							  } catch (Exception e) {
+								  e.printStackTrace();
+							  }
+
+						  }
+					  }
+				  }
+
 				  request.setAttribute("DATAMAP", res);
 
 			  } catch (NullPointerException e) {
