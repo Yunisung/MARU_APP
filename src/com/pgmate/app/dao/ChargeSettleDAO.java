e5afb04b8114eca16e98b9ecf857d007b4836704
yangwenry <yangwenry@bkwinners.com>
2024-07-30 16:54:21 +0900

    가상계좌 거래내역 엑셀다운시 계좌번호, 은행명 표시

--- src/com/pgmate/app/dao/ChargeSettleDAO.java
+++ src/com/pgmate/app/dao/ChargeSettleDAO.java
@@ -76,7 +76,7 @@ public class ChargeSettleDAO extends DAO{
 
 		super.setTable("(SELECT D.*, FN_AES_DEC(account) as decAccount, FN_AES_DEC(holder) as decHolder," +
 				"(SELECT codeName FROM PG_CODE WHERE alias='BANK' AND code=D.vactBankCd) AS vactBankName " +
-				"FROM (SELECT A.*, CASE WHEN A.trxType = '입금' THEN B.bankCd WHEN A.trxType = '출금' THEN C.bankCd END AS vactBankCd " +
+				"FROM (SELECT A.*, CASE WHEN A.trxType = '입금' THEN B.bankCd WHEN A.trxType = '출금' THEN C.bankCd END AS vactBankCd, B.account as vactAccount " +
 				"FROM VW_CHARGE_SETTLE A LEFT OUTER JOIN PG_VACT_TRX B ON A.trxId = B.vactId AND A.mchtId = B.mchtId AND A.trxType = '입금' AND A.trxUnit = '가상계좌정산' " +
 				"LEFT OUTER JOIN PG_FIRM_TRX C ON A.refId = C.idx AND A.trxType = '출금' AND A.trxUnit = '펌뱅킹' " +
 				"LEFT OUTER JOIN VW_TRX_CAP_LIST F ON A.trxId=F.trxId " +
