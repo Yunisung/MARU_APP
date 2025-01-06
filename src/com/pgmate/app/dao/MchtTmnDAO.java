dbaa903c0686de31edf0c1e1c0b875841fb402af
pys890102 <pys890102@bkwinners.com>
2025-01-06 15:17:20 +0900

    250106_수기결제 카드 제한 기능 추가

--- src/com/pgmate/app/dao/MchtTmnDAO.java
+++ src/com/pgmate/app/dao/MchtTmnDAO.java
@@ -98,7 +98,7 @@ public class MchtTmnDAO extends DAO{
 				"SELECT '' AS idx, tmnId, mchtId, taxId, status, serial, payKey, activeDate, apiMaxInstall" +
 				", webPay, appDirect, semiAuth" +
 				", refundType, van, vanIdx, ccType, description, minAmount, payLimit, limitAmount, limitStartTime" +
-				", limitEndTime, regId, regDay, regDate, '' as summary " +
+				", limitEndTime, blockCard, regId, regDay, regDate, '' as summary " +
 				"FROM PG_MCHT_TMN WHERE mchtId ='" + mchtId + "') AS HT_TMN " +
 				"LEFT OUTER JOIN PG_MCHT_TMN ON HT_TMN.tmnId = PG_MCHT_TMN.tmnId ");
 		super.setColumns("HT_TMN.*, PG_MCHT_TMN.tmnId AS originId, PG_MCHT_TMN.regDate AS activeDate ");
