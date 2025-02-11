58ff1b06704bb47d566317d5615b1276ec419507
jjggoo <kjm7943@bkwinners.com>
2025-02-11 14:03:28 +0900

    미수금 합계 금액 추가

--- src/com/pgmate/app/ctl/ChargeSettleController.java
+++ src/com/pgmate/app/ctl/ChargeSettleController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.pgmate.app.dao.*;
 import com.pgmate.lib.dao.DAO;
+import com.pgmate.lib.util.lang.CommonUtil;
 import com.pgmate.lib.util.map.SharedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,10 @@ import com.pgmate.app.model.ajax.CPRequest;
 import com.pgmate.app.util.CPRUtil;
 import com.pgmate.app.util.SessionUtil;
 import com.pgmate.lib.dao.RecordSet;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 public class ChargeSettleController {
@@ -32,12 +37,64 @@ public class ChargeSettleController {
 
 		request.setAttribute("SUMMAP", dao.depositSum(cpRequest.data).getRow(0));
 
+		if(!CommonUtil.isNullOrSpace(cpRequest.getKeyValue("mchtId"))){
+			String mAccount = dao.getMaccount(cpRequest.getKeyValue("mchtId"));
+			String trxDay = CommonUtil.getCurrentDate("yyyyMMdd");
+			String trxTime = CommonUtil.getCurrentDate("HHmmss");
+			Date trxDate = CommonUtil.getDate("yyyyMMddHHmmss", trxDay + trxTime);
+
+			String hour = trxTime.substring(0, 2);
+			String min = trxTime.substring(2, 4);
+
+			String startTrxDay = trxDay;
+			String startTrxTime = hour + "0000";
+
+			if(mAccount.equals("131022424175")){
+				if(Integer.parseInt(min) < 6) {
+					// hour - 1
+					String prevHourDate = getPrevHourDate(trxDate);
+					startTrxDay = prevHourDate.substring(0, 8);
+					startTrxTime = prevHourDate.substring(8, 10) + "0000";
+				}
+
+				request.setAttribute("OUTSANDINGSUMMAP", dao.getVactAfterSum(startTrxDay, startTrxTime, cpRequest.getKeyValue("mchtId")));
+			}
+			if(mAccount.equals("131022424199")) {
+				if(Integer.parseInt(hour) < 7) {
+					String prevDayDate = getPrevDayDate(trxDate);
+					startTrxDay = prevDayDate.substring(0, 8);
+
+					request.setAttribute("OUTSANDINGSUMMAP", dao.getVactBetweenSum(startTrxDay, cpRequest.getKeyValue("mchtId")));
+				} else {
+					startTrxDay = trxDay;
+					startTrxTime = "000000";
+					request.setAttribute("OUTSANDINGSUMMAP", dao.getVactAfterSum(startTrxDay, startTrxTime, cpRequest.getKeyValue("mchtId")));
+				}
+			}
+		}
+
 //		RecordSet rset = dao.list(cpRequest.data,cpRequest.page);
 		RecordSet rset = dao.listWithDecAccount(cpRequest.data,cpRequest.page);
 		return new CPRUtil(cpRequest).dataList(rset,dao).setView(request,"/chargeSettle/list","");
 		
 	}
-	
+
+	public static String getPrevHourDate(Date calcDate) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(calcDate);
+		cal.add(Calendar.HOUR, -1);
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyyMMddHHmmss");
+		return sdformat.format(cal.getTime());
+	}
+
+	private String getPrevDayDate(Date calcDate) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(calcDate);
+		cal.add(Calendar.DATE, -1);
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyyMMddHHmmss");
+		return sdformat.format(cal.getTime());
+	}
+
 	@RequestMapping(value = "/chargeSettle/view/{trxId}", method = RequestMethod.GET)
     public ModelAndView trxView(HttpServletRequest request, @PathVariable("trxId") String trxId) {
 		request.setAttribute("DATAMAP", new ChargeSettleDAO().getById(trxId).getRow(0));
